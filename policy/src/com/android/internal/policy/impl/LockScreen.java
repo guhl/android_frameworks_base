b0cbe633ab7790a853e8c265977999931a53e3bf
Nico Kaiser <kaiser.nico@gmail.com>
2012-07-18 14:17:21 +0200

    Fix don't interrupt ongoing PIN/PUK verification
    This fixes an issue which only occurs if
    1. you use a SIM PIN
    2. the PIN verification takes longer than the screen timeout is. e.g. when then screen turns black while "unlocking SIM" message is displayed!
    
    It might be possible to simulate the bug by pressing power quickly after the PIN has ben entered and "unlocking SIM" message is still being displayed.
    
    If the PIN verification takes longer thant the screen timeout is then the screen turns black.
    If that happens LockPatternKeyguardView.reset() is called which then forces a re-creation of the view. But then the original created
    SimUnlockScreen.onSimLockChangedResponse callback never receives the success message for the SIM unlock.
    What then happens is that the LockPatternKeyguardView is re-created again and again whenever screen turns off while waiting
    until we receive a SIM state changed event e.g. until we are back on network.
    But when that happens we are presented the pattern unlock screen. With a single touch we can then (finally) get on the homescreen.
    
    The fix removes the forced re-creation of the LockPatternKeyguardView if there is a PIN/PUK verification in progress. Which leads to a 'normal' unlock behavior:
    1. enter pin
    2. "unlocking SIM" message is displayed
    3. maybe screen turns off after timeout
    4. after some seconds you get on the homescreen directly
    
    former ICS submission: http://review.cyanogenmod.com/#/c/19243/
    
    AOSP: https://android-review.googlesource.com/#/c/32310/
    
    The issue is still present when using jellybean code.
    
    Change-Id: I1f9bb26105d07c72b59bdec064e9e93cbdc918c4

--- policy/src/com/android/internal/policy/impl/LockScreen.java
+++ policy/src/com/android/internal/policy/impl/LockScreen.java
@@ -791,6 +791,11 @@ class LockScreen extends LinearLayout implements KeyguardScreen {
     }
 
     /** {@inheritDoc} */
+    public boolean suspendRecreate() {
+        return false;
+    }
+
+    /** {@inheritDoc} */
     public boolean needsInput() {
         return false;
     }
