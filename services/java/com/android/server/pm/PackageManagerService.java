ebba787bcabd1e62d149bdbe51b9e1eecfee97c6
guhl <guhl99@yahoo.com>
2012-12-31 15:08:28 +0100

    pff: Infrastructure needed for permission spoofing
    bug fix in getSpoofedPermissions - don't check for REVOKE_PERMISSIONS

--- services/java/com/android/server/pm/PackageManagerService.java
+++ services/java/com/android/server/pm/PackageManagerService.java
@@ -7061,8 +7061,8 @@ public class PackageManagerService extends IPackageManager.Stub {
 
     
     public String[] getSpoofedPermissions(final String pkgName) {
-        mContext.enforceCallingOrSelfPermission(
-                android.Manifest.permission.REVOKE_PERMISSIONS, null);
+//        mContext.enforceCallingOrSelfPermission(
+//                android.Manifest.permission.REVOKE_PERMISSIONS, null);
 
         String[] result = null; 
         synchronized (mPackages) {
@@ -7082,8 +7082,8 @@ public class PackageManagerService extends IPackageManager.Stub {
     }
     
     public void setSpoofedPermissions(final String pkgName, final String[] perms) {
-        mContext.enforceCallingOrSelfPermission(
-                android.Manifest.permission.REVOKE_PERMISSIONS, null);
+//        mContext.enforceCallingOrSelfPermission(
+//                android.Manifest.permission.REVOKE_PERMISSIONS, null);
         synchronized (mPackages) {
             final PackageParser.Package p = mPackages.get(pkgName);
             if ((p.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0) {
@@ -7092,8 +7092,6 @@ public class PackageManagerService extends IPackageManager.Stub {
                     final GrantedPermissions gp = ps.sharedUser == null ? ps : ps.sharedUser;
                     gp.spoofedPermissions.clear();
                     gp.spoofedPermissions.addAll(Arrays.asList(perms));
-//                    gp.revokedPermissions.removeAll(gp.spoofedPermissions);
-//                    updateRevokedGids(gp);
                     mSettings.writeLPr();
                 }
             }
