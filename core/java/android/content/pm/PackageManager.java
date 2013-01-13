68c908f730141faf2772200a867a2e2025a87b4d
guhl <guhl99@yahoo.com>
2013-01-14 00:42:23 +0100

    pff: permission spoofing - location

--- core/java/android/content/pm/PackageManager.java
+++ core/java/android/content/pm/PackageManager.java
@@ -2979,7 +2979,6 @@ public abstract class PackageManager {
      * 			  uid will be returned.
      *
      * @param packageName Name of the package which revoked permissions are needed
-     * @hide
      */
 
     public abstract String[] getSpoofedPermissions(String packageName);
@@ -2991,8 +2990,7 @@ public abstract class PackageManager {
      * 			  permissions for that shared uid.
      *
      * @param packageName Name of the package which revoked permissions are needed
-     * @param the spoofed permissions.
-     * @hide
+     * @param perms the spoofed permissions.
      */
     public abstract void setSpoofedPermissions(String packageName, String[] perms);
     
