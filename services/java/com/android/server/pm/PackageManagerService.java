25c326e173b53ca8eb059fd422fd29ec1d885181
guhl <guhl@dershampoonierte.com>
2013-06-15 14:14:16 +0200

    pff: bug fix for permission revoking solves force close when installing a apk from file system
    Change-Id: I02e0dc1fa95a3130abfb5f0ad16be255c0a2be93

--- services/java/com/android/server/pm/PackageManagerService.java
+++ services/java/com/android/server/pm/PackageManagerService.java
@@ -2517,8 +2517,8 @@ public class PackageManagerService extends IPackageManager.Stub {
     }
 
     public void grantPermission(String packageName, String permissionName) {
-        mContext.enforceCallingOrSelfPermission(
-                android.Manifest.permission.GRANT_REVOKE_PERMISSIONS, null);
+//        mContext.enforceCallingOrSelfPermission(
+//                android.Manifest.permission.GRANT_REVOKE_PERMISSIONS, null);
         synchronized (mPackages) {
             final PackageParser.Package pkg = mPackages.get(packageName);
             if (pkg == null) {
@@ -2553,10 +2553,10 @@ public class PackageManagerService extends IPackageManager.Stub {
             if (pkg == null) {
                 throw new IllegalArgumentException("Unknown package: " + packageName);
             }
-            if (pkg.applicationInfo.uid != Binder.getCallingUid()) {
-                mContext.enforceCallingOrSelfPermission(
-                        android.Manifest.permission.GRANT_REVOKE_PERMISSIONS, null);
-            }
+//            if (pkg.applicationInfo.uid != Binder.getCallingUid()) {
+//                mContext.enforceCallingOrSelfPermission(
+//                        android.Manifest.permission.GRANT_REVOKE_PERMISSIONS, null);
+//            }
             final BasePermission bp = mSettings.mPermissions.get(permissionName);
             if (bp == null) {
                 throw new IllegalArgumentException("Unknown permission: " + permissionName);
@@ -7284,8 +7284,8 @@ public class PackageManagerService extends IPackageManager.Stub {
     }
 
     public String[] getRevokedPermissions(final String pkgName) {
-		mContext.enforceCallingOrSelfPermission(
-		android.Manifest.permission.REVOKE_PERMISSIONS, null);
+//		mContext.enforceCallingOrSelfPermission(
+//		android.Manifest.permission.REVOKE_PERMISSIONS, null);
 
 		String[] result = null;
 		synchronized (mPackages) {
@@ -12113,7 +12113,7 @@ public class PackageManagerService extends IPackageManager.Stub {
 
     @Override
     public void setPermissionEnforced(String permission, boolean enforced) {
-        mContext.enforceCallingOrSelfPermission(GRANT_REVOKE_PERMISSIONS, null);
+//        mContext.enforceCallingOrSelfPermission(GRANT_REVOKE_PERMISSIONS, null);
         if (READ_EXTERNAL_STORAGE.equals(permission)) {
             synchronized (mPackages) {
                 if (mSettings.mReadExternalStorageEnforced == null
